f68503dfd043aa016de36e9582a21c31771efc74
RAJVEER011989 <rajsep.1@gmail.com>
2026-08-13 22:18:13 +0530

    changed the port

--- server/index.js
+++ server/index.js
@@ -484,7 +484,7 @@ app.use((req, res, next) => {
   next()
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Truelip server running on port ${PORT}`)
